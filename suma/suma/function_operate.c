f4a36704928f0c37fbc483cc100a0f4cb0b43416
qpalzm60409 <75030040+qpalzm60409@users.noreply.github.com>
2020-12-31 22:12:50 +0800

    22:12

--- suma/suma/function_operate.c
+++ suma/suma/function_operate.c
@@ -8,33 +8,37 @@ void ParameterOperate(MainDataStut *mainData, AllegroObjStut *allegroObj)
     switch(state)
     {
     case GAME_PLAYING:
-        allegroObj->background.x -= 3;
+        allegroObj->background.x -= OFFSET_ROLE_WALK;
         if(allegroObj->background.x <= -SIZE_IMG_BKG_WIDTH) allegroObj->background.x += SIZE_IMG_BKG_WIDTH;
         /* Role*/
-        if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_W)){
-            if(allegroObj->role.state != ROLE_MUST_DROP) allegroObj->role.start_y -= 10;
+
+        if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_W))
+        {
+            if(allegroObj->role.state != ROLE_MUST_DROP) allegroObj->role.start_y -= OFFSET_ROLE_JUMP;
             if(allegroObj->role.state == ROLE_NULL) allegroObj->role.state = ROLE_JUMP;
         }
-        if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_S)){ //帶與重力修正
-            allegroObj->role.start_y += 10;
+        if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_S))  //帶與重力修正
+        {
+            allegroObj->role.start_y += OFFSET_ROLE_JUMP;
             if(allegroObj->role.state == ROLE_JUMP) allegroObj->role.state = ROLE_DROP;
         }
         if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_D))
-            allegroObj->role.start_x += 10;
+            allegroObj->role.start_x += OFFSET_ROLE_JUMP;
         if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_A))
-            allegroObj->role.start_x -=10;
+            allegroObj->role.start_x -=OFFSET_ROLE_JUMP;
         //邊界
-        if(allegroObj->role.start_y <= 550) allegroObj->role.state = ROLE_MUST_DROP;
+        if(allegroObj->role.start_y <= MAX_ROLE_Y ) allegroObj->role.state = ROLE_MUST_DROP;
         //if(allegroObj->role.state == ROLE_MUST_DROP) Gravity(allegroObj);
 
-        if(allegroObj->role.start_y >= 700){
-            allegroObj->role.start_y = 700;
+        if(allegroObj->role.start_y >= MIN_ROLE_Y)
+        {
+            allegroObj->role.start_y = MIN_ROLE_Y;
             allegroObj->role.state = ROLE_NULL;
         }
-        if(allegroObj->role.start_x < 0)
-            allegroObj->role.start_x = 0;
-        if(allegroObj->role.start_x > 1500)
-            allegroObj->role.start_x = 1500;
+        if(allegroObj->role.start_x < MIN_ROLE_X)
+            allegroObj->role.start_x = MIN_ROLE_X;
+        if(allegroObj->role.start_x > MAX_ROLE_X)
+            allegroObj->role.start_x = MAX_ROLE_X;
 
         Gravity(allegroObj); //?
         break;
@@ -45,8 +49,8 @@ void ParameterOperate(MainDataStut *mainData, AllegroObjStut *allegroObj)
 
 void Gravity(AllegroObjStut *allegroObj) //重力
 {
-    const int gravity =3;
-    allegroObj->role.start_y += gravity;
+    //const float gravity =GRAVITY;
+    allegroObj->role.start_y += GRAVITY;
 }
 
 void role_init(AllegroObjStut *allegroObj)
@@ -64,12 +68,12 @@ void role_jump(AllegroObjStut *allegroObj)
     //bool jump =true;
     if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_W))
     {
-        allegroObj->role.start_y -= 15;
+        allegroObj->role.start_y -= OFFSET_ROLE_JUMP ;
     }
     if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_D))
-        allegroObj->role.start_x += 3;
+        allegroObj->role.start_x += OFFSET_ROLE_WALK ;
     if(al_key_down(&allegroObj->keyboard_state, ALLEGRO_KEY_A))
-        allegroObj->role.start_x -=3;
+        allegroObj->role.start_x -=OFFSET_ROLE_WALK ;
 
     if(allegroObj->role.start_y> 800)
         allegroObj->role.start_y = 800;
