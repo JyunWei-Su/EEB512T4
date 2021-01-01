c8adfc059135b4ecf2e2c767e4e98fb67849afee
JyunWei-Su <43307755+JyunWei-Su@users.noreply.github.com>
2021-01-01 14:25:12 +0800

    daily update

--- suma/suma/own/header_HYH.h
+++ suma/suma/own/header_HYH.h
@@ -9,9 +9,7 @@ typedef enum Crash_State {
 } Crash_State;
 
 //====================
-void Change(float *start_1,float *end_1);
-bool CrashCheck(float start_x1,float start_y1,float end_x1,float end_y1,float start_x2,float start_y2,float end_x2,float end_y2);
-bool DefineCrash(float start_x1,float start_y1,float end_x1,float end_y1,float start_x2,float start_y2,float end_x2,float end_y2);
+
 
 #endif //_HYH_H_
 
